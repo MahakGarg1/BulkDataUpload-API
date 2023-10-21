02c21f6659e6648a63bf4e09b14a008e11b7231a
mahak.garg <sbgupta@WellDyneRx.com>
2023-10-21 22:49:50 +0530

    Modify

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ const port = 3004;
 const { Readable } = require('stream');
 const basePath ='C:\\\\Users\\pankaj garg\\Documents\\' ;
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const upload = multer({limits: {
+  fileSize: 1024 * 1024 * 1, // 1 MB limit (adjust as needed)
+}, storage: storage }).single('csvFile');
 
-app.post('/upload', upload.single('csvFile'), (req, res) => {
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      // A Multer error occurred (e.g., file too large)
+      return res.status(400).json({ error: 'File size exceeds the limit' });
+    } else if (err) {
+      // An unknown error occurred
+      return res.status(500).json({ error: 'File upload error' });
+    }
+
     if (!req.file) {
       return res.status(400).json({ error: 'No file uploaded' });
     }
@@ -38,6 +49,8 @@ app.post('/upload', upload.single('csvFile'), (req, res) => {
     
         }
       });
+  });
+    
   });
 
 app.listen(port, () => {
